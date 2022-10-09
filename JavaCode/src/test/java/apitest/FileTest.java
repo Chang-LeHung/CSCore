57e2533fa2679b03135e03024206c927c44816c0
Chang-LeHung <799207539@qq.com>
2022-10-10 02:11:18 +0800

    update test

--- JavaCode/src/test/java/apitest/FileTest.java
+++ JavaCode/src/test/java/apitest/FileTest.java
@@ -7,7 +7,10 @@ import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.nio.file.attribute.PosixFileAttributes;
 import java.util.Objects;
+
+import static java.nio.file.LinkOption.NOFOLLOW_LINKS;
 
 public class FileTest {
 
@@ -29,5 +32,24 @@ public class FileTest {
     System.out.println(Files.getAttribute(path, "unix:gid"));
     System.out.println(Files.getAttribute(path, "unix:size"));
     System.out.println(Files.getAttribute(path, "unix:nlink"));
+  }
+
+  @Test
+  public void attrTest02() throws IOException {
+    Path path = Paths.get(".");
+    PosixFileAttributes attr = Files.readAttributes(path, PosixFileAttributes.class, NOFOLLOW_LINKS);
+    System.out.println(attr.owner());
+    System.out.println(attr.group());
+    System.out.println(attr.isRegularFile());
+    System.out.println(attr.isSymbolicLink());
+    System.out.println(attr.isDirectory());
+    System.out.println(attr.isOther());
+    System.out.println(attr.permissions());
+    System.out.println(attr.lastAccessTime());
+    System.out.println(attr.creationTime());
+    System.out.println(attr.lastModifiedTime());
+    System.out.println(attr.fileKey());
+    System.out.println(attr.size());
+
   }
 }
